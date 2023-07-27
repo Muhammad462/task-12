2517752d15562ec5b0cf0b8a9d447bcc8ca9eaa8
Muhammad462 <139973126+Muhammad462@users.noreply.github.com>
2023-07-27 17:06:09 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+// task: cars list array
+var cars = ["toyota", "honda", "suzuki"];
+for (var car of cars)
+    var message = `i would like to buy a $(cars)car`;
+{
+    console.log("message");
+}
---
+++ index.ts
@@ -0,0 +1,7 @@
+// task: cars list array
+var cars = ["toyota","honda","suzuki"]
+for (var car of cars)
+var message = `i would like to buy a $(cars)car`
+{
+    console.log("message")
+}
